5fd38d4841df509b79a3d267df561d04ac962da2
ronilpatil <ronilpatil2000@gmail.com>
2024-08-07 08:17:22 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Will explore categorical encoding, discretization, missing value treatment, feature scaling, handle outliers, and feature transformation in depth',
+    author='ronilpatil',
+    license='MIT',
+)
